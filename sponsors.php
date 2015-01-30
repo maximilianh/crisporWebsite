c5df7b1e506311c46969e1224a1319b8da1ce554
Maximilian Haeussler <maximilianh@gmail.com>
2015-01-30 15:07:47 +0100

    forgot file

---
+++ sponsors.php
@@ -0,0 +1,48 @@
+
+<div class="sponsors">
+    <a title="Buy Cas9 Nuclease protein, S. pyogenes" target="_blank" href="http://international.neb.com/products/m0386-cas9-nuclease-s-pyogenes">
+        <img style="width:225px;" src="http://tefor.net/crispor/image/NEBlogo.png">
+    </a>
+</div>
+
+<style>
+      
+   div.sponsors:hover
+   {
+        -webkit-animation-play-state: paused;
+        opacity:1;
+   }
+   div.sponsors
+   {
+       -webkit-animation:           mymove 2s infinite; /* Chrome, Safari, Opera */
+       -webkit-animation-direction: alternate; /* Chrome, Safari, Opera */
+
+       animation:           mymove 2s infinite;        
+       animation-direction: alternate;
+   }
+   /* Chrome, Safari, Opera */
+   @-webkit-keyframes mymove
+   {
+       from
+       {                
+           opacity: 0.7;
+       }
+       to   
+       {             
+           opacity: 1;
+       }
+   }
+   /* Standard syntax */
+   @keyframes mymove
+   {
+       from
+       {                
+           opacity: 0.7;
+       }
+       to   
+       {             
+           opacity: 1;
+       }
+   }
+
+</style>
