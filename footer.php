18f9cae09df812f47d2b6a2719134af6b7efc597
Maximilian Haeussler <maximilianh@gmail.com>
2015-02-19 12:46:04 +0100

    style changes by JB

--- footer.php
+++ footer.php
@@ -1,4 +1,10 @@
-<div class="footer" id="footer-credits" style='display:none;background-color:rgba(300,300,300,0.95);border-radius:5px;'>
+<div class="footer" id="footer-credits" 
+     style="display:none;background-color:rgba(300,300,300,0.97);border-radius:5px;">
+    
+    <div class="linklike" style="float:left;margin-left:5px;margin-top:-15px;margin-bottom:10px;font-size:x-large;" onclick="$(this).parent().hide();">
+        hide        
+    </div>
+    
     <table class="footertable" align="center">
         <tr>
             <td></td>
