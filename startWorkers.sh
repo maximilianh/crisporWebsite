a2d083aa84d7819d383ce8e3dc588a7155da3add
Maximilian Haeussler <maximilianh@gmail.com>
2015-03-06 12:27:30 +0100

    only one worker in dev mode

--- startWorkers.sh
+++ startWorkers.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+set -e 
 if [ "$EUID" -ne 0 ]
   then echo "must be run with sudo"
   exit
 fi
-for i in `seq 4`; do
+workCount=4
+if [ `pwd` == "/var/www/crisporTest" -o `pwd` == "/var/www/crisporMax" ] ; then
+    workCount=1
+fi
+echo $workCount
+for i in `seq $workCount`; do
     log=log/worker$i.log
     ./crispor.cgi --user www-data --worker `pwd` > $log 2>&1
     echo worker $i started, logfile is $log;
