a8da624078a0f2880d492245d39ef4cbcf7689ad
Maximilian Haeussler <maximilianh@gmail.com>
2015-07-17 14:50:05 +0200

    keeping worker ID

--- startWorkers.sh
+++ startWorkers.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 set -e 
-#if [ "$EUID" -ne 0 ]
-  #then echo "must be run with sudo"
-  #exit
-#fi
-
+if [ "$EUID" -ne 0 ]
+  then echo "must be run with sudo"
+  exit
+fi
 workCount=4
-echo `pwd`
-if [ `pwd` == "/home/www/crisporTest" -o `pwd` == "/home/www/crisporMax" -o `pwd` == "/var/www/crisporMax" ] ; then
+if [ `pwd` == "/var/www/crisporTest" -o `pwd` == "/var/www/crisporMax" ] ; then
     workCount=1
 fi
 echo $workCount
 for i in `seq $workCount`; do
     log=log/worker$i.log
-    sudo bash -c "./crispor.cgi --user www-data --worker `pwd` > $log 2>&1"
-    echo worker $i started, logfile is $log;
+    date > $log
+    ./crispor.cgi --user www-data --worker `pwd` $i >> $log 2>&1
+    echo worker $i started, PID $!, logfile is $log;
 done
