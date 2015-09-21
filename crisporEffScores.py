f6fa9d24800adffd7d7dbbcaec2fc56eb87935de
Maximilian Haeussler <max@soe.ucsc.edu>
2015-09-21 14:22:33 +0200

    adding housden score, still called drsc in the code as originally we had no author name for the score calculated by the website

--- crisporEffScores.py
+++ crisporEffScores.py
@@ -5,7 +5,8 @@
 # - CrisprScan: Moreno-Mateos, Nat Meth 2015, PMID 26322839, http://crisprscan.org
 # - ssc: Xu et al, Gen Res 2015, PMID 26063738, http://crispr.dfci.harvard.edu/SSC/
 # - Chari: Chari et al, PMID 26167643 http://crispr.med.harvard.edu/sgRNAScorer
-# - Fusi: Fusi et al, prepublication manuscript on bioarxiv, http://dx.doi.org/10.1101/021568 http://research.microsoft.com/en-us/projects/azimuth/
+# - Fusi: Fusi et al, prepublication manuscript on bioarxiv, http://dx.doi.org/10.1101/021568 http://research.microsoft.com/en-us/projects/azimuth/, only available as a web API
+# - Housden: Housden et al, PMID 26350902, http://www.flyrnai.org/evaluateCrispr/
 # - OOF: Microhomology and out-of-frame score from Bae et al, Nat Biotech 2014 , PMID24972169 http://www.rgenome.net/mich-calculator/
 
 # the input are 100bp sequences that flank the basepair just 5' of the PAM +/-50bp.
@@ -17,6 +18,7 @@
 from subprocess import Popen, PIPE, STDOUT, check_output, CalledProcessError, call
 import platform, math, tempfile, bisect, sys, os, logging, types, optparse
 from os.path import dirname, join, basename, isfile, expanduser, isdir
+from math import log10
 
 import urllib2
 import json
@@ -606,27 +608,6 @@ def cacheScores(scoreName, scoreFunc, seqs):
     assert(len(allScores)==len(seqs))
     return allScores
 
-drscScores = None
-
-def lookupDrscScores(seqs):
-    """
-    DRSC http://www.flyrnai.org/evaluateCrispr/ does not provide an implementation.
-    Scores were collected manually with 
-    cat effData/*.scores.tab | cut -f3  | sort -u | cut -c-20 | grep -v seq | pbcopy
-    and then pasted into the website.
-    The results were download in excel format, saved as .tab and converted to a simpler
-    format with
-    less Output-0c4bb0fa-962b-43c6-8b8c-d7d7205ec494.txt | cut -f2,9 | grep Input -v > scores1.tab
-    """
-    global drscScores
-    if drscScores is None:
-        drscScores = readDict("effData/drsc/scores1.tab")
-
-    scores = []
-    for s in seqs:
-        scores.append(drscScores[s])
-    return scores
-
 def calcAllBaeScores(seqs):
     """
     run seqs through calcMicroHomolScore()
@@ -711,7 +692,7 @@ def calcAllScores(seqs, addOpt=[], doAll=False):
     given 100bp sequences (50bp 5' of PAM, 50bp 3' of PAM) calculate all efficiency scores
     and return as a dict scoreName -> list of scores (same order).
     >>> calcAllScores(["CCACGTCTCCACACATCAGCACAACTACGCAGCGCCTCCCTCCACTCGGAAGGACTATCCTGCTGCCAAGAGGGTCAAGTTGGACAGTGTCAGAGTCCTG"])
-    {'oof': [51], 'wang': [66], 'chariRaw': [-0.15504833], 'mh': [4404], 'finalGc6': [1], 'chariRank': [54], 'doench': [10], 'finalGg': [0], 'crisprScan': [39], 'ssc': [-0.035894]}
+    {'oof': [51], 'wang': [66], 'drsc': [6.3], 'chariRaw': [-0.15504833], 'mh': [4404], 'finalGc6': [1], 'chariRank': [54], 'doench': [10], 'finalGg': [0], 'crisprScan': [39], 'ssc': [-0.035894]}
     """
     scores = {}
 
@@ -720,6 +701,8 @@ def calcAllScores(seqs, addOpt=[], doAll=False):
             raise Exception("sequence %s is %d bp and not 100 bp long" % (s, len(s)))
 
     guideSeqs = trimSeqs(seqs, -20, 0)
+
+    scores["drsc"] = calcHousden(trimSeqs(seqs, -20, 0))
 
     scores["wang"] = cacheScores("wang", calcWangSvmScores, guideSeqs)
     if "wangOrig" in addOpt or doAll:
@@ -743,10 +726,6 @@ def calcAllScores(seqs, addOpt=[], doAll=False):
     # fusi score not run by default, requires an apiKey
     if "fusi" in addOpt or doAll:
         scores["fusi"] = cacheScores("fusi", sendFusiRequest, trimSeqs(seqs, -24, 6))
-
-    # drsc score not run by default, requires that scores are available in a tab-sep file
-    if "drsc" in addOpt or doAll:
-        scores["drsc"] = lookupDrscScores(trimSeqs(seqs, -20, 0))
 
     return scores
 
@@ -795,6 +774,38 @@ def readSeqs(inFname):
             continue
         filtSeqs.append(s)
     return filtSeqs
+
+# Housden matrix (see function below):
+# an array of 4x20=80 floats. The first 20 are for A, the next 20 for T, then C, then G
+# imported from original file received from the authors: matrix_final.txt
+factors = [
+    0.4979, 0.7959, 0.7553, 0.6569, 0.9481, 0.7147, 0.437, 0.6212, 0.9077, 1.0, 0.1957, 0.7959, 0.6212, 0.8912, 1.0, 0.5485, 0.9942, 0.5485, 0.455, 1.0, \
+    0.6699, 0.5485, 0.275, 0.5972, 0.6212, 0.7555, 1.0, 0.5131, 0.8608, 0.7553, 0.6569, 0.3417, 1.0, 0.016, 0.9146, 0.7555, 0.2906, 0.4979, 0.5485, 0.5131, 
+    0.4979, 0.6869, 0.8528, 0.7643, 0.5325, 0.3417, 0.3417, 0.7643, 0.6434, 0.0092, 0.9331, 0.5325, 0.7272, 0.9708, 0.2905, 0.7272, 0.2957, 0.7918, 0.6434, 0.5062, \
+    0.7918, 0.4461, 0.4851, 0.4461, 0.3417, 0.6869, 0.2417, 0.5485, 0.0947, 0.9256, 0.5325, 0.8308, 0.1255, 0.7918, 0.2544, 0.4461, 0.4979, 0.6212, 0.7918, 0.4461
+]
+
+def calcHousden(seqs):
+    """
+    Calc housden score and round to one decimal point.
+    Based on java file Crispr.java received from the authors.
+    >>> calcHousden(["ATCTGACCTCCCGGCTAATT"])
+    [6.9]
+    """
+    scores = []
+    for seq in seqs:
+        assert(len(seq)==20)
+        nuclToIndex = {"A":0,"T":1,"C":2,"G":3}
+
+        score = 1.0
+        for i in range(0, 20):
+            nuclIndex = nuclToIndex[seq[i]]
+            idx = (nuclIndex*20)+i
+            score *= factors[idx]
+        score = -1*log10(score)
+        score = float("%0.1f" % score) # round to one decimal point
+        scores.append(score)
+    return scores
 
 # ----------- MAIN --------------
 if __name__=="__main__":
