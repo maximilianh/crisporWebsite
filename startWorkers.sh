06acea5d8133f1e8eb40c577e1ac4df239f209f0
Maximilian Haeussler <maximilianh@gmail.com>
2017-04-28 20:29:34 +0200

    adding checks for running workers

--- startWorkers.sh
+++ startWorkers.sh
@@ -9,7 +9,8 @@ echo $workCount
 mkdir -p log
 for i in `seq $workCount`; do
     log=log/worker$i.log
-    date > $log
+    echo "---- NEW LOG ----" >> $log
+    date >> $log
     ./crispor.py --worker `pwd` $i >> $log 2>&1
     echo worker $i started, PID $!, logfile is $log;
 done
