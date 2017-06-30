727ca22ff9682005dab354dc0377057f381f13b5
Maximilian Haeussler <maximilianh@gmail.com>
2017-06-30 09:31:09 +0200

    fixing pLentiCrispr overhang bug and removing the top menu for now

--- crispor.py
+++ crispor.py
@@ -258,7 +258,7 @@ addGenePlasmidInfo = {
 "43860" : ("ACACC", "AAAAC", "G", "BsmBI", "https://www.addgene.org/static/data/plasmids/43/43860/43860-attachment_T35tt6ebKxov.pdf"),
 "49330" : ("TTC", "AAC", "", "Bsp QI", "http://bio.biologists.org/content/3/1/42#sec-9"),
 "42230" : ("CACC", "AAAC", "", "Bbs1", "https://www.addgene.org/static/data/plasmids/52/52961/52961-attachment_B3xTwla0bkYD.pdf"),
-"52961" : ("CACC", "AAAC", "G", "BsmBI", "https://www.addgene.org/static/data/plasmids/52/52961/52961-attachment_B3xTwla0bkYD.pdf"),
+"52961" : ("CACC", "AAAC", "", "BsmBI", "https://www.addgene.org/static/data/plasmids/52/52961/52961-attachment_B3xTwla0bkYD.pdf"),
 "61591" : ("CACC", "AAAC", "", "BsaI", "https://www.addgene.org/static/data/plasmids/61/61591/61591-attachment_it03kn5x5O6E.pdf"),
 "61592" : ("CACC", "AAAC", "", "BsaI", "https://www.addgene.org/static/data/plasmids/61/61592/61592-attachment_iAbvIKnbqNRO.pdf"),
 "61593" : ("CACC", "AAAC", "", "BsaI", "https://www.addgene.org/static/data/plasmids/61/61592/61592-attachment_iAbvIKnbqNRO.pdf"),
@@ -2312,8 +2312,8 @@ def printHeader(batchId, title):
 <meta property='fb:admins' content='692090743' />
 <meta name="google-site-verification" content="OV5GRHyp-xVaCc76rbCuFj-CIizy2Es0K3nN9FbIBig" />
 <meta property='og:type' content='website' />
-<meta property='og:url' content='http://tefor.net/crispor/crispor.py' />
-<meta property='og:image' content='http://tefor.net/crispor/image/CRISPOR.png' />
+<meta property='og:url' content='http://crispor.org/' />
+<meta property='og:image' content='http://crispor.tefor.net/image/CRISPOR.png' />
 
 """
 
@@ -3234,7 +3234,7 @@ def printForm(params):
     print """
 <form id="main-form" method="post" action="%s">
 
-<br><div style="padding: 2px; margin-bottom: 10px; border: 1px solid black; background-color:white">June 2017: CRISPOR Batch for lentiviral screens, saturation-mutagenesis and Genbank export now in the <a href="http://tefor.net/crisporDev/crisporBeta/crispor.py">beta of Crispor V4.3</a>.
+<br><div style="padding: 2px; margin-bottom: 10px; border: 1px solid black; background-color:white">June 2017: CRISPOR Batch for lentiviral screens, saturation-mutagenesis and Genbank export now in the <a href="http://crispor.tefor.net/crisporDev/crisporBeta/crispor.py">beta of Crispor V4.3</a>.
 </div>
 
 
@@ -3720,7 +3720,7 @@ def showSeqDownloadMenu(batchId):
     htmls.append(html)
 
     #myUrl = "http://crispor.tefor.net/"+cgiGetSelfUrl({"download":"genomecompiler"})
-    myUrl = "http://tefor.net/crisporDev/crisporMax/crispor.py?batchId=%s&download=genomecompiler" % batchId
+    myUrl = "http://crispor.tefor.net/crispor.py?batchId=%s&download=genomecompiler" % batchId
     #backUrl = "https://designer.genomecompiler.com/plasmid_iframe?file_url=%s#/plasmid" % urllib.quote(myUrl)
     backUrl = "https://designer.genomecompiler.com/plasmid_iframe?file_url=%s#/plasmid" % urllib.quote(myUrl)
     html = "<a target=_blank href='%s'>GenomeCompiler</a>" % backUrl
@@ -3931,20 +3931,20 @@ def printTeforBodyStart():
     print """<div style="float:left">"""
     #print """<a href='http://genome.ucsc.edu'><img style='vertical-align: top; height: 40px' src='%s/image/ucscBioinf.jpg' alt=''></a>""" % (HTMLPREFIX)
     print "</div>"
-    #<div style="position: absolute; left:50px; top:30px"><a href='http://tefor.net/'><img style='width:60px' src='%simage/logo_tefor.png' alt=''></a></div>
-    print """ %s
-<div id='navi' style="margin-top:0; margin-bottom:0; margin-left:120px">
-        <div id='menu' class='default'>
-                <ul class='navi'>
-                <li><a href='http://tefor.net'>Home</a></li>
-                <li><a href='./crispor.py'>CRISPOR</a></li>
-                <li><a href='http://tefor.net/pages/services/overview.php#'>Services</a></li>
-                <li><a href='http://tefor.net/pages/partners'>Partners</a></li>
-                <li><a href='http://tefor.net/pages/contacts'>Contacts</a></li>
-                </ul>
-        </div>
-</div>
-    """ % HTMLPREFIX
+    #print ("""<div style="position: absolute; left:50px; top:30px"><a href='http://tefor.net/'><img style='width:60px' src='%simage/logo_tefor.png' alt=''></a></div>""" % HTMLPREFIX)
+    #print """ %s
+#<div id='navi' style="margin-top:0; margin-bottom:0; margin-left:120px">
+        #<div id='menu' class='default'>
+                #<ul class='navi'>
+                #<li><a href='http://tefor.net'>Home</a></li>
+                #<li><a href='./crispor.py'>CRISPOR</a></li>
+                #<li><a href='http://tefor.net/pages/services/overview.php#'>Services</a></li>
+                #<li><a href='http://tefor.net/pages/partners'>Partners</a></li>
+                #<li><a href='http://tefor.net/pages/contacts'>Contacts</a></li>
+                #</ul>
+        #</div>
+#</div>
+    #""" % HTMLPREFIX
 
     print '<div id="bd">'
     print '<div class="centralpanel" style="margin-left:0px">'
@@ -5771,7 +5771,7 @@ def printCloningSection(batchId, primerGuideName, guideSeq, params):
         ]
 
     print "<h3 id='gibson'>Cloning with Gibson assembly into pLenti-puro</h3>"
-    print ("""Order the following oligonucleotide to clone with Gibson assemly into the vector <a href='https://www.addgene.org/52963/'>pLenti-puro</a>. The exact protocol for this is in preparation by Matt Canver.""")
+    print ("""Order the following oligonucleotide to clone with Gibson assembly into the vector <a href='https://www.addgene.org/52963/'>pLenti-puro</a>. The exact protocol for this is in preparation by Matt Canver.""")
     lentiPrimers = [
         ("batchOligo%s" % primerGuideName, "<i>GGAAAGGACGAAACACCG</i>"+guideSeq+"<i>GTTTTAGAGCTAGAAATAGCAAGTTAAAATAAGGC</i>"),
     ]
