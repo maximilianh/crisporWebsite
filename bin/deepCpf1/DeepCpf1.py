912e75ff53f8948a4582b407f394241c2a3db17e
Maximilian Haeussler <maximilianh@gmail.com>
2018-04-10 16:26:37 -0700

    deepcpf1 update

--- bin/deepCpf1/DeepCpf1.py
+++ bin/deepCpf1/DeepCpf1.py
@@ -1,7 +1,6 @@
 # original source from https://github.com/MyungjaeSong/Paired-Library
 # modified by Max Haeussler for better integration into CRISPOR
 # original code was retained, just commented out
-# I am not using the model that requires chromatin data
 
 from os.path import dirname, join
 from numpy import *
@@ -43,46 +42,49 @@ def scoreSeqs(seqs):
     Seq_deepCpf1_Output = Dense(1, activation='linear')(Seq_deepCpf1_DO4)
     Seq_deepCpf1 = Model(inputs=[Seq_deepCpf1_Input_SEQ], outputs=[Seq_deepCpf1_Output])
     
-    #DeepCpf1_Input_SEQ = Input(shape=(34,4))
-    #DeepCpf1_C1 = Convolution1D(80, 5, activation='relu')(DeepCpf1_Input_SEQ)
-    #DeepCpf1_P1 = AveragePooling1D(2)(DeepCpf1_C1)
-    #DeepCpf1_F = Flatten()(DeepCpf1_P1)
-    #DeepCpf1_DO1= Dropout(0.3)(DeepCpf1_F)
-    #DeepCpf1_D1 = Dense(80, activation='relu')(DeepCpf1_DO1)
-    #DeepCpf1_DO2= Dropout(0.3)(DeepCpf1_D1)
-    #DeepCpf1_D2 = Dense(40, activation='relu')(DeepCpf1_DO2)
-    #DeepCpf1_DO3= Dropout(0.3)(DeepCpf1_D2)
-    #DeepCpf1_D3_SEQ = Dense(40, activation='relu')(DeepCpf1_DO3)
+    DeepCpf1_Input_SEQ = Input(shape=(34,4))
+    DeepCpf1_C1 = Convolution1D(80, 5, activation='relu')(DeepCpf1_Input_SEQ)
+    DeepCpf1_P1 = AveragePooling1D(2)(DeepCpf1_C1)
+    DeepCpf1_F = Flatten()(DeepCpf1_P1)
+    DeepCpf1_DO1= Dropout(0.3)(DeepCpf1_F)
+    DeepCpf1_D1 = Dense(80, activation='relu')(DeepCpf1_DO1)
+    DeepCpf1_DO2= Dropout(0.3)(DeepCpf1_D1)
+    DeepCpf1_D2 = Dense(40, activation='relu')(DeepCpf1_DO2)
+    DeepCpf1_DO3= Dropout(0.3)(DeepCpf1_D2)
+    DeepCpf1_D3_SEQ = Dense(40, activation='relu')(DeepCpf1_DO3)
     
-    #DeepCpf1_Input_CA = Input(shape=(1,))
-    #DeepCpf1_D3_CA = Dense(40, activation='relu')(DeepCpf1_Input_CA)
-    #DeepCpf1_M = Multiply()([DeepCpf1_D3_SEQ, DeepCpf1_D3_CA])
+    DeepCpf1_Input_CA = Input(shape=(1,))
+    DeepCpf1_D3_CA = Dense(40, activation='relu')(DeepCpf1_Input_CA)
+    DeepCpf1_M = Multiply()([DeepCpf1_D3_SEQ, DeepCpf1_D3_CA])
     
-    #DeepCpf1_DO4= Dropout(0.3)(DeepCpf1_M)
-    #DeepCpf1_Output = Dense(1, activation='linear')(DeepCpf1_DO4)
-    #DeepCpf1 = Model(inputs=[DeepCpf1_Input_SEQ, DeepCpf1_Input_CA], outputs=[DeepCpf1_Output])
+    DeepCpf1_DO4= Dropout(0.3)(DeepCpf1_M)
+    DeepCpf1_Output = Dense(1, activation='linear')(DeepCpf1_DO4)
+    DeepCpf1 = Model(inputs=[DeepCpf1_Input_SEQ, DeepCpf1_Input_CA], outputs=[DeepCpf1_Output])
     
     #print "Loading weights for the models"
     myDir = dirname(__file__)
     Seq_deepCpf1.load_weights(join(myDir, 'weights/Seq_deepCpf1_weights.h5'))
-    #DeepCpf1.load_weights(join(myDir, 'weights/DeepCpf1_weights.h5'))
+    DeepCpf1.load_weights(join(myDir, 'weights/DeepCpf1_weights.h5'))
     
     #print "Loading test data"
     #FILE = open(sys.argv[1], "r")
     #data = FILE.readlines()
     #SEQ, CA = PREPROCESS(data)
-    SEQ = PREPROCESS(seqs)
+    SEQ, CA0, CA1 = PREPROCESS(seqs)
     #FILE.close()
     
     #print "Predicting on test data"
     #Seq_deepCpf1_SCORE = Seq_deepCpf1.predict([SEQ], batch_size=50, verbose=0)
-    scores = Seq_deepCpf1.predict([SEQ], batch_size=50, verbose=0)
+    seqScores = Seq_deepCpf1.predict([SEQ], batch_size=50, verbose=0)
     #DeepCpf1_SCORE = DeepCpf1.predict([SEQ, CA], batch_size=50, verbose=0) * 3
-    
-    res = []
-    for s in scores:
-        res.append(s[0])
-    return res
+    deepScores0 = DeepCpf1.predict([SEQ, CA0], batch_size=50, verbose=0) * 3
+    deepScores1 = DeepCpf1.predict([SEQ, CA1], batch_size=50, verbose=0) * 3
+    # Max: why "* 3" ??
+
+    seqScores = [x[0] for x in seqScores] # remove numpy objects, I like normal numbers
+    deepScores0 = [x[0] for x in deepScores0]
+    deepScores1 = [x[0] for x in deepScores1]
+    return seqScores, deepScores0, deepScores1
     #print "Saving to " + sys.argv[2]
     #OUTPUT = open(sys.argv[2], "w")
     #res = []
@@ -99,7 +101,8 @@ def PREPROCESS(seqs):
     data_n = len(seqs)
     #data_n = len(lines) - 1
     SEQ = zeros((data_n, 34, 4), dtype=int)
-    #CA = zeros((data_n, 1), dtype=int)
+    CA0 = zeros((data_n, 1), dtype=int)
+    CA1 = zeros((data_n, 1), dtype=int)
     
     #for l in range(1, data_n+1):
     for l in range(0, data_n):
@@ -118,10 +121,11 @@ def PREPROCESS(seqs):
             elif seq[i] in "Tt":
                 SEQ[l, i, 3] = 1
                 #SEQ[l-1, i, 3] = 1
-        #CA[l,0] = int(data[2])
+        #CA[l-1,0] = int(data[2])
+        CA0[l,0] = 1
+        CA1[l,0] = 1
 
-    #return SEQ, CA
-    return SEQ
+    return SEQ, CA0, CA1
 
 #if __name__ == '__main__':
         #main()
