138c4e43e6e8f0391c62f09ffa3b4f48f15e7b69
Maximilian Haeussler <maximilianh@gmail.com>
2018-05-30 22:52:38 +0200

    trying to fix startup

--- startWorkers.sh
+++ startWorkers.sh
@@ -11,7 +11,8 @@ for i in `seq $workCount`; do
     log=log/worker$i.log
     echo "---- NEW LOG ----" >> $log
     date >> $log
-    ./crispor.py --worker `pwd` $i >> $log 2>&1
+    pwd=`pwd`
+    nohup ./crispor.py --worker $pwd $i >> $log 2>&1
     echo $! > log/worker$i.pid
     echo worker $i started, PID $!, logfile is $log;
 done
